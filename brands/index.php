c856b71e868efe067b32d9bdb219c7bfead89cbd
iRoge <iroge-27@yandex.ru>
2021-08-12 00:39:13 +0300

    SEO

--- brands/index.php
+++ brands/index.php
@@ -3,7 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Бренды");
 $APPLICATION->SetPageProperty("keywords", DEFAULT_KEYWORDS);
 $APPLICATION->SetPageProperty("description", "Список брендов в Городе Оргазма. В Городе Оргазма более 700 производителей и брендов товаров для взрослых. Lelo, Calexotics, Baile, Doc Johnson, Dream Toys, Erolanta, Le Frivole, Leg Avenue, Lola toys, Lovetoys, Obsessive, Orion, Pipedream, Passion, NS Novelties, Livia Corsetti, Leg Avenue");
-$APPLICATION->SetPageProperty("title", 'Список брендов в Городе Оргазма');
 
 $APPLICATION->IncludeComponent(
     "rdevs:brands",
@@ -13,6 +12,7 @@ $APPLICATION->IncludeComponent(
         "CACHE_TIME" => "36000000",
     ),
     false
-); ?>
+);
 
-<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+$APPLICATION->SetPageProperty("title", 'Список брендов в Городе Оргазма');
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
