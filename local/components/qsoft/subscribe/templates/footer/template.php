d840c6b942d5571d71c0e3a7a5c429f2d6bdd258
iRoge <iroge-27@yandex.ru>
2021-08-23 16:12:34 +0300

    gfjh

--- local/components/qsoft/subscribe/templates/footer/template.php
+++ local/components/qsoft/subscribe/templates/footer/template.php
@@ -10,16 +10,23 @@
     <? else : ?>
     <div class="subscribe-wrapper">
         <div class="main">
-            <form class="col-lg-3" id="subscribe-form" novalidate method="post">
-                <?= bitrix_sessid_post() ?>
-                <input type="hidden" name="action" value="subscribe">
-                <input type="hidden" name="subscribe" value="Y">
-                <input type="email" class="js-footer-email" name="EMAIL"
-                       placeholder="Введите ваш e-mail" required autocomplete="off">
-                <input type="submit" value="Подписаться">
-                <ul class="subscribe-errors"></ul>
-            </form>
-<!--                <img class="col-lg-3 subscribe-second-img" src="--><?//=SITE_TEMPLATE_PATH?><!--/img/subscribe2.webp" alt="Подписка">-->
+            <div class="col-lg-6 col-md-9 subscribe-left-wrapper">
+                <img class="subscribe-first-img" src="<?=SITE_TEMPLATE_PATH?>/img/subscribe1.webp" alt="Подписка">
+            </div>
+            <div class="col-lg-4 col-md-3 subscribe-center-wrapper">
+                <form id="subscribe-form" novalidate method="post">
+                    <?= bitrix_sessid_post() ?>
+                    <input type="hidden" name="action" value="subscribe">
+                    <input type="hidden" name="subscribe" value="Y">
+                    <input type="email" class="js-footer-email" name="EMAIL"
+                           placeholder="Ваша электронная почта" required autocomplete="off">
+                    <input type="submit" value="Подписаться">
+                    <ul class="subscribe-errors"></ul>
+                </form>
+            </div>
+            <div class="col-lg-2 hidden-xs hidden-md hidden-sm subscribe-right-wrapper">
+                <img class="subscribe-second-img" src="<?=SITE_TEMPLATE_PATH?>/img/subscribe2.webp" alt="Подписка 2">
+            </div>
         </div>
     </div>
     <? endif; ?>
