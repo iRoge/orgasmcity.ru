359238dd43cbfc6ec8025ba0b0b3f43311c4a0b6
iRoge <iroge-27@yandex.ru>
2021-08-23 16:32:05 +0300

    Доделал форму подписки

--- local/components/qsoft/subscribe/templates/footer/template.php
+++ local/components/qsoft/subscribe/templates/footer/template.php
@@ -3,17 +3,29 @@
 } ?>
 <div class="js-subscribe-new">
     <? if (!empty($arResult['MESSAGE'])) : ?>
-        <div class="subscribe-message">
-            <i class="icon icon-check"></i>
-            <b><?= $arResult['MESSAGE']; ?></b>
+    <div class="subscribe-wrapper">
+        <div class="main">
+            <div class="col-lg-6 col-md-9 col-sm-9 subscribe-left-wrapper">
+                <img class="subscribe-first-img" src="<?=SITE_TEMPLATE_PATH?>/img/subscribe1.webp" alt="Подписка">
+            </div>
+            <div class="col-lg-4 col-md-3 col-sm-3 subscribe-center-wrapper">
+                <div class="subscribe-message">
+                    <i class="icon icon-check"></i>
+                    <b><?= $arResult['MESSAGE']; ?></b>
+                </div>
+            </div>
+            <div class="col-lg-2 hidden-xs hidden-md hidden-sm subscribe-right-wrapper">
+                <img class="subscribe-second-img" src="<?=SITE_TEMPLATE_PATH?>/img/subscribe2.webp" alt="Подписка 2">
+            </div>
         </div>
+    </div>
     <? else : ?>
     <div class="subscribe-wrapper">
         <div class="main">
-            <div class="col-lg-6 col-md-9 subscribe-left-wrapper">
+            <div class="col-lg-6 col-md-9 col-sm-9 subscribe-left-wrapper">
                 <img class="subscribe-first-img" src="<?=SITE_TEMPLATE_PATH?>/img/subscribe1.webp" alt="Подписка">
             </div>
-            <div class="col-lg-4 col-md-3 subscribe-center-wrapper">
+            <div class="col-lg-4 col-md-3 col-sm-3 subscribe-center-wrapper">
                 <form id="subscribe-form" novalidate method="post">
                     <?= bitrix_sessid_post() ?>
                     <input type="hidden" name="action" value="subscribe">
