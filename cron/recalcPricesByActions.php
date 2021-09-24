cce39a0c6f56f05b20bac05d23749433b0642164
iRoge <45058118+iRoge@users.noreply.github.com>
2021-09-24 18:32:10 +0300

    SAD

--- cron/recalcPricesByActions.php
+++ cron/recalcPricesByActions.php
@@ -6,7 +6,42 @@ while (ob_get_level()) {
 }
 set_time_limit(0);
 ini_set('memory_limit', '2048M');
+
+$nowDate = date('d.m.Y H:i:s');
+$rsItems = CIBlockElement::GetList(
+    [],
+    [
+        'IBLOCK_ID' => IBLOCK_GROUPS,
+        'PROPERTY_IS_ACTION' => true,
+        [
+            "LOGIC" => "OR",
+            ["<=DATE_ACTIVE_FROM" => $nowDate, ">=DATE_ACTIVE_TO" => $nowDate, "ACTIVE" => 'N'],
+            [">DATE_ACTIVE_FROM" => $nowDate, "ACTIVE" => 'Y'],
+            ["<DATE_ACTIVE_TO" => $nowDate, "ACTIVE" => 'Y'],
+        ],
+    ],
+    false,
+    false,
+    [
+        'ID',
+        'IBLOCK_ID',
+        'CODE',
+        'ACTIVE',
+        'NAME'
+    ]
+);
+
+while ($action = $rsItems->GetNext()) {
+    $el = new CIBlockElement();
+    $el->Update(
+        $action['ID'],
+        [
+            'ACTIVE' => ($action['ACTIVE'] == 'N' ? 'Y' : 'N')
+        ]
+    );
+}
+
 \Qsoft\Helpers\PriceUtils::recalcPrices();
 global $CACHE_MANAGER;
-$CACHE_MANAGER->ClearByTag("catalogAll");
+$CACHE_MANAGER->ClearByTag("pricesAll");
 \Qsoft\Helpers\PriceUtils::getCachedPriceForUser(1);
