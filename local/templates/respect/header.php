44c21df3cba11dd270bec1f17b5a90c5e43dd6a6
iRoge <iroge-27@yandex.ru>
2021-10-15 00:46:46 +0300

    Update header.php

--- local/templates/respect/header.php
+++ local/templates/respect/header.php
@@ -189,7 +189,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
             clickmap:true,
             trackLinks:true,
             accurateTrackBounce:true,
-            webvisor: false,
+            webvisor: true,
             ecommerce:"metrikaData"
         });
     </script>
