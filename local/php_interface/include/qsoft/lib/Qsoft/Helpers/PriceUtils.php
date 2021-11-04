0534991207a7a8f22f432b0b0ae0e6b679d03d4b
iRoge <iroge-27@yandex.ru>
2021-11-04 15:18:05 +0300

    qwe

--- local/php_interface/include/qsoft/lib/Qsoft/Helpers/PriceUtils.php
+++ local/php_interface/include/qsoft/lib/Qsoft/Helpers/PriceUtils.php
@@ -252,15 +252,15 @@ class PriceUtils
         if ($markupPercent >= 150) {
             $price = self::calculatePrice($rrcPrice, 25);
         } elseif ($markupPercent >= 135) {
-            $price = self::calculatePrice($rrcPrice, 20);
+            $price = self::calculatePrice($rrcPrice, 25);
         } elseif ($markupPercent >= 120) {
-            $price = self::calculatePrice($rrcPrice, 15);
+            $price = self::calculatePrice($rrcPrice, 20);
         } elseif ($markupPercent >= 85) {
-            $price = self::calculatePrice($rrcPrice, 10);
+            $price = self::calculatePrice($rrcPrice, 15);
         } elseif ($markupPercent >= 65) {
-            $price = self::calculatePrice($rrcPrice, 5);
+            $price = self::calculatePrice($rrcPrice, 10);
         } else {
-            $price = self::calculatePrice($rrcPrice, 0);
+            $price = self::calculatePrice($rrcPrice, 5);
         }
 
         return $price;
